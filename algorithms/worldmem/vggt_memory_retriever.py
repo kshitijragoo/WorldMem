1493cce339cbe70210a5842b1596cad09ff1ad06
kshitijragoo <109583287+kshitijragoo@users.noreply.github.com>
2025-09-28 16:34:19 +0800

    file directoryq

--- algorithms/worldmem/vggt_memory_retriever.py
+++ algorithms/worldmem/vggt_memory_retriever.py
@@ -11,7 +11,7 @@ from dataclasses import dataclass
 from concurrent.futures import ThreadPoolExecutor
 
 # Add VGGT to path - using vggt directory
-sys.path.append("../../../vggt")
+
 from vggt.models.vggt import VGGT
 from vggt.utils.load_fn import load_and_preprocess_images
 from vggt.utils.pose_enc import pose_encoding_to_extri_intri
